eb06dbe2a0bad4a67cf96d58213d2a350d8f0144
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2021-01-30 16:59:00 +0100

    Added logtable to db script

--- DatabaseScripts/createdb.sql
+++ DatabaseScripts/createdb.sql
@@ -16,3 +16,13 @@ CREATE TABLE `tokens` (
   `created` datetime NOT NULL,
   PRIMARY KEY (`userid`,`token`)
 );
+
+DROP TABLE IF EXISTS `logtable`;
+CREATE TABLE `logtable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `loglevel` int(11) NOT NULL,
+  `groupname` varchar(100) NOT NULL DEFAULT '',
+  `message` varchar(1024) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+);
