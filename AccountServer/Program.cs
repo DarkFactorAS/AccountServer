d57430f5d9e9adce0244ac86e81bdbcd1912f482
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2021-03-14 21:43:29 +0100

    Move program and startup to server

---
+++ AccountServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using DFCommonLib.Config;
+using DFCommonLib.Logger;
+using DFCommonLib.Utils;
+
+using AccountServer.Repository;
+
+namespace AccountServer
+{
+    class Program
+    {
+        public static string AppName = "AccountServer";
+
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+
+                new DFServices(services)
+                    .SetupLogger()
+                    .SetupMySql()
+                    .LogToConsole(DFLogLevel.INFO)
+                    .LogToMySQL(DFLogLevel.WARNING)
+                    .LogToEvent(DFLogLevel.ERROR, AppName);
+                ;
+
+                services.AddTransient<IAccountRepository, AccountRepository>();
+                services.AddTransient<IAccountProvider, AccountProvider>();
+                services.AddTransient<IConfigurationHelper, ConfigurationHelper>();
+
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }
+        );
+    }
+}
