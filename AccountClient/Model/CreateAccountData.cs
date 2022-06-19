cbea3b9438162b84be1ff7b72204a2effa3714d8
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2022-06-19 21:12:24 +0200

    BOTS-186 Create account with email

--- AccountClient/Model/CreateAccountData.cs
+++ AccountClient/Model/CreateAccountData.cs
@@ -8,6 +8,7 @@ namespace AccountClientModule.Model
         public string username { get; set; }
         public string password { get; set; }
         public string nickname { get; set; }
+        public string email { get; set; }
 
         public CreateAccountData()
         {
