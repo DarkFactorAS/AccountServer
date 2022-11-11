79360423c47877862db262b214a1b3ef72ec09e6
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2022-11-11 19:30:14 +0100

    BOTS-222 Proper messages for account errors

--- AccountServer/Model/ReturnData/ReturnData.cs
+++ AccountServer/Model/ReturnData/ReturnData.cs
@@ -36,7 +36,6 @@ namespace AccountServer.Model
             this.message = message;
         }
 
-
         public static ReturnData OKMessage( string message = "OK" )
         {
             return new ReturnData(ReturnCode.OK, message);
@@ -51,8 +50,31 @@ namespace AccountServer.Model
         {
             var data = new ReturnData();
             data.code = errorCode;
-            data.message = Enum.GetName( typeof( ReturnCode ), errorCode );
+            data.message = GetMessageFromId(errorCode);
             return data;
+        }
+
+        public static String GetMessageFromId( ReturnCode code )
+        {
+            switch(code)
+            {
+                case UserDoesNotExist: return "User does not exist";
+                case UserAlreadyExist: return "User already exist";
+                case ErrorInData: return "Error in the data";
+                case TokenLoginError: return "Token login error";
+                case WrongPassword: return "Wrong password";
+                case InvalidUsername: return "Wrong password";
+                case UsernameInvalidCharacters: return "Username contaisn valid characters";
+                case NicknameInvalidCharacters: return "Nickname contains valid characters";
+                case PasswordInvalidCharacters: return "Password contains invalid characters. User A-Z, 0-9";
+                case NicknameAlreadyExist: return "Nickname already exist";
+                case NotValidEmail: return "Not a valid email";
+                case SessionTimedOut: return "Code verification timed out. Please try again";
+                case FailWithMailServer: return "Could not send code";
+
+                default:
+                    return Enum.GetName( typeof( ReturnCode ), code );
+            }
         }
     }
 }
