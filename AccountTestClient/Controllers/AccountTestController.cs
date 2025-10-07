1777cec29de35cdd25aaac33fcfc262563c59985
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2025-10-07 08:22:43 +0200

    CoPilot Review: Controller fixes on test class

--- AccountTestClient/Controllers/AccountTestController.cs
+++ AccountTestClient/Controllers/AccountTestController.cs
@@ -6,21 +6,23 @@ using TestAccountClient;
 
 namespace TestAccountClient
 {
-
+    [ApiController]  
+    [Route("[controller]")]
     public class AccountTestController : ControllerBase
     {
         private readonly IAccountClientProgram _client;
 
-        public AccountTestController(IAccountClientProgram client)
+        public AccountTestController(IAccountClientProgram client) : base()
         {
             _client = client;
         }
 
         [HttpGet]
         [Route("RunTests")]
-        public void RunTests()
+        public IActionResult RunTests()
         {
             _client.Run();
+            return Ok();
         }
     }
 }
